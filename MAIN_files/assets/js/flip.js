d1d2830dfee3b7eefa53e858f00f3cc231682983
Yashakk <yashashree.kulkarni05@gmail.com>
2021-03-01 15:41:14 -0500

    project1.submission

--- MAIN_files/assets/js/flip.js
+++ MAIN_files/assets/js/flip.js
@@ -1,18 +1,11 @@
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             var flat = $("#flat").flipster({
-            style: 'flat',
-            spacing: -0.2,
-            // start specifies the start index, default is 'center'
-            // start: 0,
-            // set loop to true for it to keep playing, default is false
-            loop: false,
+                style: 'flat',
+                spacing: -0.25,
+                loop: false,
             });
-            // Can research flipster functions at https://github.com/drien/jquery-flipster/
-            flat.flipster('play', 2000)
-            flat.flipster('index')
-            
-            // jQuery to remove, clone, and add images from the thumbnail as they are clicked
+        
             $("#flat img").click(function(){
                 if ($("#flat img").hasClass("active")) {
                     $("#flat img").removeClass("active");
